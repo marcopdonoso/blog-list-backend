0368e15ef6a48d9f5950ff77a331c6ec72f6efd3
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-04-10 13:20:08 -0400

    refactor the application - exercise 4.2

---
+++ app.js
@@ -0,0 +1,19 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
