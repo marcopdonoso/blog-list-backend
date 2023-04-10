1c1d24bcfc5cf28188fffbc6bcc38e285f0bfe56
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-04-10 16:23:23 -0400

    totalLikes test

--- utils/list-helper.js
+++ utils/list-helper.js
@@ -2,6 +2,16 @@ const dummy = blogs => {
 	return 1
 }
 
+const totalLikes = blogs => {
+	let likesAcc = 0
+	blogs.map(blog => {
+		likesAcc += blog.likes
+	})
+
+	return likesAcc
+}
+
 module.exports = {
 	dummy,
+	totalLikes,
 }
