b7d6621bffc15b9b5d43b4c36a2d665cf7805225
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-04-10 22:27:27 -0400

    aux functions and unit tests

--- utils/list-helper.js
+++ utils/list-helper.js
@@ -1,4 +1,6 @@
-const dummy = blogs => {
+const _ = require('lodash')
+
+const dummy = () => {
 	return 1
 }
 
@@ -11,7 +13,54 @@ const totalLikes = blogs => {
 	return likesAcc
 }
 
+const favoriteBlog = blogs => {
+	const reducer = (prev, curr) => {
+		return prev.likes > curr.likes ? prev : curr
+	}
+
+	const mostLikedBlog =
+		blogs.length === 0
+			? { title: 'None', author: 'None', likes: 0 }
+			: blogs.reduce(reducer)
+
+	const { title, author, likes } = mostLikedBlog
+
+	return { title, author, likes }
+}
+
+const mostBlogs = blogs => {
+	const blogsCounterByAuthor = _.countBy(blogs, 'author')
+	const authorNames = Object.keys(blogsCounterByAuthor)
+	const numberOfBlogs = Object.values(blogsCounterByAuthor)
+	const authorsArray = []
+
+	authorNames.forEach((authorName, i) => {
+		const authorObject = {}
+		authorObject.author = authorName
+		authorObject.blogs = numberOfBlogs[i]
+		authorsArray.push(authorObject)
+	})
+
+	const mostBlogsAuthor = _.maxBy(authorsArray, 'blogs')
+
+	return authorNames.length === 0
+		? { author: 'None', blogs: 0 }
+		: mostBlogsAuthor
+}
+
+const mostLikes = blogs => {
+	const mostLikedAuthor =
+		blogs.length === 0 ? { author: 'None', likes: 0 } : _.maxBy(blogs, 'likes')
+
+	const { author, likes } = mostLikedAuthor
+
+	return { author, likes }
+}
+
 module.exports = {
 	dummy,
 	totalLikes,
+	favoriteBlog,
+	mostBlogs,
+	mostLikes,
 }
