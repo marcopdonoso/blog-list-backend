c7cb48c5a63dd687fda87d67734f5d24f486ef92
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-04-17 12:31:01 -0400

    delete a blog functionality implemented and tested

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -88,6 +88,22 @@ test('blog without title or url is not added', async () => {
 	expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 })
 
+test('succeeds deletion of a blog with valid id', async () => {
+	const blogsAtStart = await helper.blogsInDb()
+
+	const blogToDelete = blogsAtStart[0]
+
+	await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+	const blogsAtEnd = await helper.blogsInDb()
+
+	expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+
+	const titles = blogsAtEnd.map(b => b.title)
+
+	expect(titles).not.toContain(blogToDelete.title)
+})
+
 afterAll(() => {
 	mongoose.connection.close()
 })
