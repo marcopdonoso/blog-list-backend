75cf537b8abc597fcd2d23384808ba36ae12de11
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-04-19 19:52:44 -0400

    refactor to create middleware that extracts token from Authorization header and puts it in request token field

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ mongoose
 app.use(cors())
 app.use(express.json())
 app.use(middleWare.requestLogger)
+app.use(middleWare.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
